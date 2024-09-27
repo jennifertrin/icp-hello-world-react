6d38cb7c91bdef84f859a5696bf3774c747af90f
Jennifer Tran <jennifer.tran@dfinity.org>
2024-09-27 16:17:26 -0700

    chore: remove extra punctuation

--- src/hello_world_react_frontend/src/components/NameInput.jsx
+++ src/hello_world_react_frontend/src/components/NameInput.jsx
@@ -46,7 +46,7 @@ const NameInput = () => {
                     </Card> :
                     <Card className="rounded-xl shadow-2xl shadow-blue-500/20 w-full h-full transition-opacity duration-500 transform backface-hidden">
                         <div className="rounded-[20px] py-12 bg-infinite px-4 h-full w-full flex flex-col justify-center items-center">
-                            <h2 className="text-3xl font-bold text-white mb-4">{greeting}!</h2>
+                            <h2 className="text-3xl font-bold text-white mb-4">{greeting}</h2>
                             <p className="mb-6 text-gray-300">Thank you for submitting your name.</p>
                             <button onClick={() => setName(null)} className="px-4 py-2 bg-picton-blue rounded-md focus:outline-none focus:ring-2 focus:ring-picton-blue focus:ring-opacity-50 hover:bg-picton-blue transition duration-300 ease-in-out font-semibold shadow-lg hover:shadow-xl hover:shadow-picton-blue/30">
                                 Go Back
